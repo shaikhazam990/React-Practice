9cfff1fea64c4d0da38d8d7129186b862f624271
shaikhazam990 <shaikhazam0990@gmail.com>
2026-02-10 11:47:08 +0530

    card using binding

--- formhandler/src/App.jsx
+++ formhandler/src/App.jsx
@@ -4,12 +4,24 @@ import React, { useState } from 'react';
 const App = () => {
 
   const [title, setTitle] = useState('')
+  const [email, setEmail] = useState('')
+
+  const [alluser, setAlluser] = useState([])
 
   const formhandler= (e)=>{
     e.preventDefault()
     console.log(title)
 
+    const oldUser = [...alluser]
+
+    oldUser.push({title,email})
+
+    console.log(oldUser)
+
+    setAlluser(oldUser)
+
     setTitle('')
+    setEmail('')
 
   }
 
@@ -26,9 +38,26 @@ const App = () => {
         onChange={(e)=>{ setTitle(e.target.value)}}
 
        />
+        <input type="text" 
+        placeholder='enter email'
+        value={email}
+        required
+        onChange={(e)=>{ setEmail(e.target.value)}}
+
+       />
 
         <button>Submit</button>
       </form>
+
+      {alluser.map((elem,indx)=>{
+
+        return <div key={indx}>
+          <h1>{elem.title}</h1>
+          <p>{elem.email}</p>
+
+        </div>
+
+      })}
     </div>
   )
 }
